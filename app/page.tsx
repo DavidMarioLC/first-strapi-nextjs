1948d3fc83661c428e49bb00a2a4a8f3c448e7e6
DavidMarioLC <davidmariolc.dev@gmail.com>
2026-03-04 08:49:43 -0500

    Update API URL environment variable in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,10 @@ const query = gql`
 `;
 
 export default async function Home() {
-  const { articles } = await request(process.env.STRAPI_API_URL || "", query);
+  const { articles } = await request(
+    process.env.NEXT_PUBLIC_API_URL || "",
+    query,
+  );
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
